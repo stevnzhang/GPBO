e5065d841d55fa2934000bada553932b33a0efd1
stevnzhang <roboticdraqo@gmail.com>
2022-04-03 17:15:10 -0400

    finished sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  include AppHelpers::Cart
+
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:username], params[:password])
+    if user
+      session[:user_id] = user.id
+      create_cart
+      redirect_to home_path, notice: "Logged in!"
+    else
+      flash.now.alert = "Username and/or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    destroy_cart
+    redirect_to home_path, notice: "Logged out!"
+  end
+
+end
