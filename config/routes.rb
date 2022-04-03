36686a5b234b4cdefea2b983747f4f6d7ff6df70
stevnzhang <roboticdraqo@gmail.com>
2022-04-03 17:17:51 -0400

    added routes to intial controllers

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,27 @@ Rails.application.routes.draw do
     end
   end
 
+  # Semi-static page routes
+  get 'home', to: 'home#index', as: :home
+  get 'home/about', to: 'home#about', as: :about
+  get 'home/contact', to: 'home#contact', as: :contact
+  get 'home/privacy', to: 'home#privacy', as: :privacy
+  get 'home/search', to: 'home#search', as: :search
+
+  # Authentication routes
+  get 'login', to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
   
+  resources :sessions, :only => [:new, :create, :destroy]
+  resources :customers, :except => [:destroy]
+  resources :addresses
+  resources :orders
+  resources :categories, :except => [:show, :destroy]
+  resources :items
+  resources :orders
+  resources :users, :except => [:show, :destroy]
+
+  patch 'items/:id/toggle_active', to: 'items#toggle_active', as: :toggle_active
+  patch 'items/:id/toggle_feature', to: 'items#toggle_feature', as: :toggle_feature
 
 end
